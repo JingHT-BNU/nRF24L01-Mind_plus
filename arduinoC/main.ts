edf00eeee78c863e169b0f4ab34e0b7b491c1958
Jort <JingHT@mail.bnu.edu.cn>
2026-05-20 09:06:56 +0800

    nRF24L01

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -1,143 +1,116 @@
-
-enum SIZE {
-    //% block="29*29"
-    1,
-    //% block="58*58"
-    2
-}
-
-enum LINE {
-    //% block="1"
-    1,
-    //% block="2"
-    2,
-    //% block="3"
-    3,
-    //% block="4"
-    4
-}
-
-enum BTN {
-    //% block="A"
-    A,
-    //% block="B"
-    B,
-    //% block="A+B"
-    AB
-}
-
-
-//% color="#AA278D" iconWidth=50 iconHeight=40
-namespace oled12864 {
-    //% block="when press [BUTTON]" blockType="hat"
-    //% BUTTON.shadow="dropdown" BUTTON.options="BTN" BUTTON.defl="BTN.A"
-    export function buttonPress(parameter: any, block: any) {
-        let button = parameter.BUTTON.code;
-        button = replace(button);
-        let name = 'button' + button + 'PressCallback';
-        if(Generator.board === 'microbit'){
-            Generator.addEvent(name, "void", name, "", true);
-            Generator.addSetup(block.id, `onEvent(ID_BUTTON_${button}, PRESS, ${name});`, false);
-        }else{
-            Generator.addInclude('MPython', '#include <MPython.h>');
-            Generator.addEvent(name, "void", name, "", true);
-            Generator.addSetupMainTop("mPython.begin", "mPython.begin();");
-            Generator.addSetup(`button${button}.setPressedCallback`, `button${button}.setPressedCallback(${name});`);
-        }
+//% color="#2196F3" iconWidth=50 iconHeight=40
+namespace nRF24L01 {
+    // 定义功率选项的枚举
+    export enum Power {
+        //% block="最小"
+        MIN = "RF24_PA_MIN",
+        //% block="低"
+        LOW = "RF24_PA_LOW",
+        //% block="高"
+        HIGH = "RF24_PA_HIGH",
+        //% block="最大"
+        MAX = "RF24_PA_MAX"
     }
 
-    //% block="show [STR] on the [LINE] line" blockType="command"
-    //% STR.shadow="string" STR.defl=hello
-    //% LINE.shadow="dropdownRound" LINE.options="LINE" LINE.defl="LINE.1"
-    export function println(parameter: any, block: any) {
-        let str = parameter.STR.code
-        let line = parameter.LINE.code
-        Generator.addInclude('oled12864', '#include <oled12864.h>');
-        Generator.addObject(`myoled`, `OLED_12864`, `myoled;`);
-        Generator.addSetup(`myoled.begin`, `myoled.begin();`);
-        Generator.addCode(`myoled.setCursorLine(${line});\n\tmyoled.printLine(${str});`);
+    export enum LEDstate {
+        //% block="亮"
+        ON = "HIGH",
+        //% block="灭"
+        OFF = "LOW"
     }
 
-    //% block="show [STR] at x [X] y [Y]" blockType="command"
-    //% STR.shadow="string" STR.defl=hello
-    //% X.shadow="range" X.params.min=0 X.params.max=127 X.defl=0
-    //% Y.shadow="range" Y.params.min=0 Y.params.max=63 Y.defl=0
-    export function print(parameter: any, block: any) {
-        let str = parameter.STR.code
-        let x = parameter.X.code
-        let y = parameter.Y.code
-        Generator.addInclude('oled12864', '#include <oled12864.h>');
-        Generator.addObject(`myoled`, `OLED_12864`, `myoled;`);
-        Generator.addSetup(`myoled.begin`, `myoled.begin();`);
-        Generator.addCode(`myoled.setCursor(${x}, ${y});\n\tmyoled.print(${str});`);
+    //% block="无线电初始化 CE引脚 [CEPIN] CSN引脚 [CSNPIN]" blockType="command"
+    //% CEPIN.shadow="dropdown" CEPIN.options="PIN_DigitalWrite"
+    //% CSNPIN.shadow="dropdown" CSNPIN.options="PIN_DigitalWrite"
+    export function radioBegin(parameter: any, block: any) {
+        let ce = parameter.CEPIN.code;
+        let csn = parameter.CSNPIN.code;
+
+        Generator.addInclude('nRF24L01_RF24', '#include <RF24.h>');
+        Generator.addObject(`radio`, `RF24`, `radio(${ce}, ${csn});`);
+        Generator.addSetup(`radio.begin`, `radio.begin();`);
     }
 
-    //% block="display QR code [STR] at x [X] y [Y] with size [SIZE]" blockType="command"
-    //% STR.shadow="string" STR.defl=http://mindplus.cc
-    //% X.shadow="range" X.params.min=0 X.params.max=127 X.defl=0
-    //% Y.shadow="range" Y.params.min=0 Y.params.max=63 Y.defl=0
-    //% SIZE.shadow="dropdownRound" SIZE.options="SIZE" SIZE.defl="SIZE.2"
-    export function qrcode(parameter: any, block: any) {
-        let str = parameter.STR.code
-        let x = parameter.X.code
-        let y = parameter.Y.code
-        let size = parameter.SIZE.code
-        Generator.addInclude('oled12864', '#include <oled12864.h>');
-        Generator.addObject(`myoled`, `OLED_12864`, `myoled;`);
-        Generator.addSetup(`myoled.begin`, `myoled.begin();`);
-        Generator.addCode(`myoled.qrcode(${x}, ${y}, ${str}, ${size});`);
+    //% block="无线电设置地址长度 [LEN]" blockType="command"
+    //% LEN.shadow="range" LEN.params.min=3 LEN.params.max=5 LEN.defl=5
+    export function setAddressWidth(parameter: any, block: any) {
+        let len = parameter.LEN.code;
+        Generator.addCode(`radio.setAddressWidth(${len});`);
     }
 
-    //% block="set the line width to [WIDTH] pixels" blockType="command"
-    //% WIDTH.shadow="range" WIDTH.params.min=1 WIDTH.params.max=128 WIDTH.defl=1
-    export function setLineWidth(parameter: any, block: any) {
-        let width = parameter.WIDTH.code
-        Generator.addInclude('oled12864', '#include <oled12864.h>');
-        Generator.addObject(`myoled`, `OLED_12864`, `myoled;`);
-        Generator.addSetup(`myoled.begin`, `myoled.begin();`);
-        Generator.addCode(`myoled.setLineWidth(${width});`);
+    //% block="无线电功率 [POW]" blockType="command"
+    //% POW.shadow="dropdown" POW.options="Power"
+    export function setPALevel(parameter: any, block: any) {
+        let pow = parameter.POW.code;
+        Generator.addCode(`radio.setPALevel(${pow});`);
     }
 
-    //% block="get the line width" blockType="reporter"
-    export function getLineWidth(parameter: any, block: any) {
-        Generator.addInclude('oled12864', '#include <oled12864.h>');
-        Generator.addObject(`myoled`, `OLED_12864`, `myoled;`);
-        Generator.addSetup(`myoled.begin`, `myoled.begin();`);
-        Generator.addCode(`myoled.getLineWidth()`);
+    //% block="无线电开始写入, 地址 [ADR]" blockType="command"
+    //% ADR.shadow="normal" ADR.defl="Node1"
+    export function openWritingPipe(parameter: any, block: any) {
+        let adr = parameter.ADR.code;
+        Generator.addCode(`radio.openWritingPipe(${adr});`);
+        Generator.addCode(`radio.stopListening();`);
     }
 
-    //% block="button [BUTTON] is pressed?" blockType="boolean"
-    //% Flag.shadow="boolean"
-    //% BUTTON.shadow="dropdown" BUTTON.options="BTN" BUTTON.defl="BTN.A"
-    export function buttonIsPressed(parameter: any, block: any) {
-        let button = parameter.BUTTON.code.replace("+", "");
-        let code;
-        if(Generator.board === 'microbit'){
-            if (button === 'A') {
-                code = `Button_A.isPressed() && !Button_B.isPressed()`;
-            } else if (button === 'B') {
-                code = `Button_B.isPressed() && !Button_A.isPressed()`;
-            } else {
-                code = `Button_AB.isPressed()`;
-            }
-            Generator.addCode([code, Generator.ORDER_UNARY_POSTFIX]);
-        }else{
-            code = `button${button}.isPressed()`;
-            Generator.addInclude('MPython', '#include <MPython.h>');
-            Generator.addSetupMainTop("mPython.begin", "mPython.begin();");
-            Generator.addCode([code, Generator.ORDER_UNARY_POSTFIX]);
-        }
+    //% block="无线电开始读取, 地址 [ADR]" blockType="command"
+    //% ADR.shadow="string" ADR.defl="Node1"
+    export function openReadingPipe(parameter: any, block: any) {
+        let adr = parameter.ADR.code;
+        Generator.addCode(`radio.openReadingPipe(0, ${adr});`);
+        Generator.addCode(`radio.startListening();`);
     }
 
-    //% block="not [Flag]" blockType="boolean"
-    //% Flag.shadow="boolean"
-    export function notTrue(parameter: any) {
-        console.log("notTrue==", parameter);
-        let code: string = '!' + (parameter.Flag.code || 'false') + '';
+    //% block="创建状态数组 [NAME] 长度 [LEN]" blockType="command"
+    //% NAME.shadow="normal" NAME.defl="data"
+    //% LEN.shadow="number" LEN.defl="5"
+    export function createArray(parameter: any, block: any) {
+        let name = parameter.NAME.code;
+        let len = parameter.LEN.code;
+        Generator.addCode(`char ${name}[${len}];`);
+    }
+
+    //% block="状态数组 [NAME] 的第 [IND] 个数据是 [VAL] ？" blockType="boolean"
+    //% NAME.shadow="normal" NAME.defl="data"
+    //% IND.shadow="number" IND.defl="0"
+    //% VAL.shadow="string" VAL.defl="1"
+    export function checkArrayValue(parameter: any, block: any) {
+        let name = parameter.NAME.code;
+        let ind = parameter.IND.code;
+        let val = parameter.VAL.code;
+        let code = `( ${name}[${ind}] == ${val} )`;
         Generator.addCode([code, Generator.ORDER_UNARY_POSTFIX]);
     }
 
-    function replace(str :string) {
-        return str.replace("+", "");
+    //% block="将状态数组 [NAME] 的第 [IND] 个数据设为 [VAL]" blockType="command"
+    //% NAME.shadow="normal" NAME.defl="data"
+    //% IND.shadow="number" IND.defl="0"
+    //% VAL.shadow="string" VAL.defl="1"
+    export function setArrayValue(parameter: any, block: any) {
+        let name = parameter.NAME.code;
+        let ind = parameter.IND.code;
+        let val = parameter.VAL.code;
+        Generator.addCode(`${name}[${ind}] = ${val};`);
+    }
+
+    //% block="使引脚 [PIN] 的LED灯 [STATE]" blockType="command"
+    //% PIN.shadow="dropdown" PIN.options="PINX" PIN.defl="PINX.P0"
+    //% STATE.shadow="dropdown" STATE.options="LEDstate" STATE.defl="LEDstate.ON"
+    export function controlLED(parameter: any, block: any) {
+        let pin = parameter.PIN.code;
+        let state = parameter.STATE.code;
+
+        Generator.addSetup(`pinmode_${pin}`, `pinMode(${pin}, OUTPUT);`);
+        Generator.addCode(`digitalWrite(${pin}, ${state});`);
+    }
+
+    //% block="引脚 [PIN] 的按钮按下？" blockType="boolean"
+    //% PIN.shadow="dropdown" PIN.options="PINX" PIN.defl="PINX.P0"
+    export function readButton(parameter: any, block: any) {
+        let pin = parameter.PIN.code;
+
+        Generator.addSetup(`pinmode_${pin}`, `pinMode(${pin}, INPUT_PULLUP);`);
+        let code = `( digitalRead(${pin}) == LOW )`;
+        Generator.addCode([code, Generator.ORDER_UNARY_POSTFIX]);
     }
 }
